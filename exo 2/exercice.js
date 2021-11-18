e9bf77091c7ea926f9cb6ab9971c9ac9df6354e1
Olivier02500 <olivier.peuchet@hotmail.com>
2021-11-18 10:22:00 +0100

    make exo 72

--- exo 2/exercice.js
+++ exo 2/exercice.js
@@ -1,11 +1,7 @@
 
+let name = document.getElementsByTagName('h1');
+console.log(name.length)
 
-
-
-
-
-
-
-
-
-
+for (let i = 0; i < name.length; i++){
+    name[i].innerHTML = "Hello, je suis ";
+}
--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,10 +1,8 @@
 
+let card = document.getElementsByClassName('name-tag');
 
-
-
-
-
-
-
-
-
+for (let i = 0; i < card.length; i++){
+    let add = document.createElement('p');
+    add.innerHTML = "HELLO WORLD !"
+    card[i].appendChild(add);
+}
